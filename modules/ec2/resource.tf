e2b9d78ca5072c60d4b63195237ff31660962a11
Thormie Harshey <ashayeadetomiwa77@gmail.com>
2025-02-27 22:38:52 +0100

    fixed variable issue

--- modules/ec2/resource.tf
+++ modules/ec2/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "first_EC2" {
   ami           = data.aws_ami.ubuntu_ami.id
   instance_type = var.instance_type["dev"]
-  subnet_id     = var.vpc.subnet_id
+  subnet_id     = var.subnet_id
 
   tags = {
     Name = local.name
