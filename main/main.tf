9cee614b2f471615b439510051b2a27608252ff1
Thormie Harshey <ashayeadetomiwa77@gmail.com>
2025-02-27 17:40:44 +0100

    updated terraform.yml

--- main/main.tf
+++ main/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
-  source = "../modules/vpc"
+  source = "./modules/vpc"
 }
 
 module "ec2" {
-  source = "../modules/ec2"
+  source = "./modules/ec2"
   subnet_id = module.vpc.subnet_id
 }
